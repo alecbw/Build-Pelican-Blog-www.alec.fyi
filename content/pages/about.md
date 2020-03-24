58356869e1a7217e18f814635a8a663b38145828
alecbw <abarrett.wilsdon@gmail.com>
2020-03-23 17:11:13 -0700

    updated pelicanconf, publishconf, added example about page and admin funcitonality for cms

---
+++ content/pages/about.md
@@ -0,0 +1,15 @@
+---
+title: about
+---
+a bit about (my work):
+
+a bit about (my life):
+
+[Email](https://gmail.com/)
+
+[LinkedIn](https://www.linkedin.com/in/abarrettwilsdon)
+
+
+[Twitter](https://twitter.com/Contextify1)
+
+[Github](https://github.com/alecbw)
--- pelicanconf.py
+++ pelicanconf.py
@@ -2,20 +2,34 @@
 # -*- coding: utf-8 -*- #
 from __future__ import unicode_literals
 
-AUTHOR = u'wer'
-SITENAME = u'wef'
+AUTHOR = "AUTHORNAME"
+SITENAME = "SITENAME"
+# If your site is available via HTTPS, make sure SITEURL begins with https://
 SITEURL = ''
 
 PATH = 'content'
+STATIC_PATHS = ["uploads", "admin", "images"]
 
-TIMEZONE = 'Europe/Paris'
+# The CMS Viewport Template
+TEMPLATE_PAGES = {"admin/index.html": "admin/index.html"}
 
-DEFAULT_LANG = u'en'
+TIMEZONE = "America/Los_Angeles"
+DEFAULT_LANG = "en"
 
 PATH = "content"
 STATIC_PATHS = ["uploads", "admin", "images"]
 
+# Whether the home (index) page will paginate
 DEFAULT_PAGINATION = False
+# Overwrites the output directory (/output) when generating new files
+DELETE_OUTPUT_DIRECTORY = True
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
+
+# path/to/theme/folder or name or installed via pelican-themes
+# THEME = "themes/pelican-sober"
+
+# Change to True if you want RSS
+FEED_ALL_ATOM = False
+FEED_ALL_RSS = False
